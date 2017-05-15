f9769301ca748dd44d5bbeab27d0af0569a87a0e
Samuel Bancroft <samuelbancroft@hotmail.com>
2017-05-15 20:26:15 +0100

    Added a few more lines, still familiarising with Git checkouts.

--- lessonone.py
+++ lessonone.py
@@ -1,2 +1,4 @@
-__author__ = 'Samuel Bancroft'
-print("The first lesson in Python 3")
+__author__ = 'Samuel Bancroft'                      # Not needed just nice to show authorship
+print("The first lesson in Python")                 # print statement to output text
+print(1 + 2)                                        # Python is clever enough to convert integers to text after adding
+
