9bb67d3fc6d742d85f6e9ddeb3d859b88c59bf12
Samuel Bancroft <samuelbancroft@hotmail.com>
2017-05-16 12:50:06 +0100

    Finalised the basic explanation of string, int and variables

--- lessonone.py
+++ lessonone.py
@@ -11,9 +11,11 @@ print("I said \"test it first\"\n" * 2)             # Same as last 2 using \n to
 # IF you start a string with " you need to finish it with " same with '
 print("Concatenate (join)" + " strings")            # Python knows the + means to join the strings together
 saying = "I said 'Test it first'"                   # create a variable holding the saying called saying!
-print(saying + " that's what I said")               # Print the string in saying and join more text at the end
+print(saying+" that's what I said")                 # Print the string in saying and join more text at the end
 isaid = "that's what I said"                        # create a variable to hold the text from the above example
-print(saying + " " + isaid)                         # Print the strings in the variables together with a space between
+print(saying+" "+isaid)                             # Print the strings in the variables together with a space between
+numberOfTimes = 2                                   # A variable holding the integer 6, the name is in camelCase
+print((saying+" "+isaid+"\n")*numberOfTimes)        # Prints the same as line 10 but using variables instead
 # Adding these comments to your code, is always a good idea. It helps others (and you) to know what code does, you will
 # forget at some point - especially after you write your 100th program :-).
 '''
