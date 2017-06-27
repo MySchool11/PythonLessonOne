c2d3a3ee8ae43deecb90af1f98df6be40e6411ae
Samuel Bancroft <samuelbancroft@hotmail.com>
2017-06-27 22:40:44 +0100

    fixed minor comment misplacement

--- lessonone.py
+++ lessonone.py
@@ -1,4 +1,4 @@
-__author__ = 'Mr Bancroft'                      # Not needed just nice to show authorship
+__author__ = 'Mr Bancroft'                          # Not needed just nice to show authorship
 
 print("The first lesson in Python")                 # print statement to output text
 print(1 + 2)                                        # Python is clever enough to convert integers to text after adding
